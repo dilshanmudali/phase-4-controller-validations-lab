ba3c2fef5352f6349e07d7ca026a781167fd1c9b
Dilshan <dilshan.mudali@gmail.com>
2021-11-03 20:40:36 -0400

    commit tree 2dbffefc4ba6f30346b569b8fb9754019ab70e86

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :category, inclusion: {in: ['Fiction', 'NonFiction']}
+    validates :content, length: {minimum: 100}
 end
